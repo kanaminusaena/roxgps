4cb26585cac78de2e4d9907ee1b39f60244795fa
jqssun <jqssun+git@googlemail.com>
2025-02-28 15:15:21 +0000

    support gms builds

--- app/src/full/java/io/github/jqssun/gpssetter/ui/MapActivity.kt
+++ app/src/full/java/io/github/jqssun/gpssetter/ui/MapActivity.kt
@@ -19,86 +19,61 @@ import io.github.jqssun.gpssetter.utils.ext.getAddress
 import io.github.jqssun.gpssetter.utils.ext.showToast
 import kotlinx.coroutines.launch
 
-import org.maplibre.android.MapLibre
-import org.maplibre.android.camera.CameraUpdateFactory
-import org.maplibre.android.maps.MapLibreMap
-import org.maplibre.android.maps.OnMapReadyCallback
-import org.maplibre.android.maps.SupportMapFragment
-import org.maplibre.android.geometry.LatLng
-import org.maplibre.android.annotations.Marker
-import org.maplibre.android.annotations.MarkerOptions
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
 
 typealias CustomLatLng = LatLng
 
-class MapActivity: BaseMapActivity(), OnMapReadyCallback, MapLibreMap.OnMapClickListener {
+class MapActivity: BaseMapActivity(), OnMapReadyCallback, GoogleMap.OnMapClickListener {
 
-    private lateinit var mMap: MapLibreMap
+    private lateinit var mMap: GoogleMap
     private var mLatLng: LatLng? = null
     private var mMarker: Marker? = null
 
     override fun hasMarker(): Boolean {
-        // TODO: if (!mMarker?.isVisible!!){
-        if (mMarker != null) {
+        if (!mMarker?.isVisible!!) {
             return true
         }
         return false
     }
     private fun updateMarker(it: LatLng) {
-        // TODO: mMarker?.isVisible = true
-        if (mMarker == null) {
-            mMarker = mMap.addMarker(
-                MarkerOptions().position(it)
-            )
-        } else {
-            mMarker?.position = it!!
-        }
+        mMarker?.position = it!!
+        mMarker?.isVisible = true
     }
     private fun removeMarker() {
-        mMarker?.remove() // mMarker?.isVisible = false
-        mMarker = null
+        mMarker?.isVisible = false
     }
     override fun initializeMap() {
-        MapLibre.getInstance(this)
-        // val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         val mapFragment = SupportMapFragment.newInstance()
         supportFragmentManager.beginTransaction()
             .replace(R.id.map, mapFragment)
             .commit()
         mapFragment?.getMapAsync(this)
     }
-    override fun moveMapToNewLocation(moveNewLocation: Boolean) {
-        if (moveNewLocation) {
-            mLatLng = LatLng(lat, lon)
-            mLatLng.let { latLng ->
-                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng!!, 12.0f.toDouble()))
-                mMarker?.apply {
-                    position = latLng
-                    // TODO:
-                    // isVisible = true
-                    // showInfoWindow()
-                }
-            }
-        }
-    }
-    override fun onMapReady(mapLibreMap: MapLibreMap) {
-        mMap = mapLibreMap
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
         with(mMap){
-            setStyle("https://demotiles.maplibre.org/style.json")
-            // TODO: mapType = viewModel.mapType
+            mapType = viewModel.mapType
 
             val zoom = 12.0f
             lat = viewModel.getLat
             lon  = viewModel.getLng
             mLatLng = LatLng(lat, lon)
             mLatLng.let {
-                updateMarker(it!!)
-                // TODO:
-                // .draggable(false).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)
-                // .visible(false)
-                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(it, zoom.toDouble()))
+                mMarker = addMarker(
+                    MarkerOptions().position(it!!).draggable(false)
+                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)).visible(false)
+                )
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(it, zoom))
             }
             setPadding(0,80,0,170)
-            addOnMapClickListener(this@MapActivity)
+            setOnMapClickListener(this@MapActivity)
             if (viewModel.isStarted){
                 mMarker?.let {
                     // TODO:
@@ -108,7 +83,7 @@ class MapActivity: BaseMapActivity(), OnMapReadyCallback, MapLibreMap.OnMapClick
             }
         }
     }
-    override fun onMapClick(latLng: LatLng): Boolean {
+    override fun onMapClick(latLng: LatLng) {
         mLatLng = latLng
         mMarker?.let { marker ->
             mLatLng.let {
@@ -119,7 +94,19 @@ class MapActivity: BaseMapActivity(), OnMapReadyCallback, MapLibreMap.OnMapClick
                 lon = it.longitude
             }
         }
-        return true
+    }
+    override fun moveMapToNewLocation(moveNewLocation: Boolean) {
+        if (moveNewLocation) {
+            mLatLng = LatLng(lat, lon)
+            mLatLng.let { latLng ->
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng!!, 12.0f))
+                mMarker?.apply {
+                    position = latLng
+                    isVisible = true
+                    showInfoWindow()
+                }
+            }
+        }
     }
 
 
